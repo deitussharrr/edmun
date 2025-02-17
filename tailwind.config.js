99b5a586c921e54dc452e18d68f88f18f629cf5c
deitussharrr <bruthacool@gmail.com>
2025-02-18 00:47:18 +0530

    fix aacha?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or, if you're using a `src` directory instead of `app` and `pages`:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
