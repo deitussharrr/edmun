b010b068ccd1c8c0097f69ff4a781baee7b6c1f3
deitussharrr <bruthacool@gmail.com>
2025-02-18 20:40:07 +0530

    Revert "fix aacha?"
    This reverts commit 99b5a586c921e54dc452e18d68f88f18f629cf5c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or, if you're using a `src` directory instead of `app` and `pages`:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
